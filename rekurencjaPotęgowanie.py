8b50c64521934a5ef0f8c3954d76bfe10687fd92
01110010pl <39325718+01110010pl@users.noreply.github.com>
2020-01-13 13:02:22 +0100

    Update rekurencjaPotęgowanie.py

--- rekurencjaPotęgowanie.py
+++ rekurencjaPotęgowanie.py
@@ -1,7 +1,7 @@
 def potegowanie(x, y):
     if y >= 0:
         if y == 0: return 1
-        else: return  potegowanie(x, y - 1)*x
+        return  potegowanie(x, y - 1)*x
     else:
         return 0
     
